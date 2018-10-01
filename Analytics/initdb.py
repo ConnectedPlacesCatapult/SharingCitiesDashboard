984a36bca168dc09c78f71cb435c18c809746066
Hemanshu <hemanshu@c17sn98bgvc1.fcc.pri>
2018-10-01 15:30:24 +0100

    added support for postgresql and automated builds

--- Analytics/initdb.py
+++ Analytics/initdb.py
@@ -4,9 +4,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Analytics.app import create_app
 import sqlalchemy
 
-
+sql = 'mysql+pymysql'
+psql = 'postgresql+psycopg2'
 app = create_app()
-db_uri = 'mysql+pymysql://%s:%s@%s/' % (app.config['DB_USERNAME'], app.config['DB_PASSWORD'], app.config['DB_HOST'])
+db_uri = '%s://%s:%s@%s/' % (psql, app.config['DB_USERNAME'], app.config['DB_PASSWORD'], app.config['DB_HOST'])
 engine = sqlalchemy.create_engine(db_uri)
 conn = engine.connect()
 conn.execute("commit")
--- Analytics/inittb_operations.py
+++ Analytics/inittb_operations.py
@@ -5,12 +5,14 @@ from Analytics.app import create_app
 import sqlalchemy
 
 
+sql = 'mysql+pymysql'
+psql = 'postgresql+psycopg2'
+
 app = create_app()
-db_uri = 'mysql+pymysql://%s:%s@%s/' % (app.config['DB_USERNAME'], app.config['DB_PASSWORD'], app.config['DB_HOST'])
+db_uri = '%s://%s:%s@%s/%s' % (psql, app.config['DB_USERNAME'], app.config['DB_PASSWORD'], app.config['DB_HOST'], app.config['DATABASE_NAME'])
 engine = sqlalchemy.create_engine(db_uri)
 conn = engine.connect()
 conn.execute("commit")
-conn.execute("use analytics;")
 conn.execute("insert into operation values (1, 'regression', now());")
 conn.execute("insert into operation values (2, 'classification', now());")
 conn.execute("insert into operation values (3, 'clustering', now());")
