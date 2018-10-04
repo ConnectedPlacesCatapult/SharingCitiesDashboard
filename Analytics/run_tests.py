00aaeea6cb7c1d65b513408ff4d4a1ec76f3df3e
Hemanshu <hemanshu@c17sn98bgvc1.fcc.pri>
2018-10-04 12:40:19 +0100

    remove rows or cols if dataframe contain NaN or blanks

--- Analytics/run_tests.py
+++ Analytics/run_tests.py
@@ -3,6 +3,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
 from resources.test_analytics import AnalyticsTest
+from resources.test_missing_values import TestMissingValues
 
 if __name__ == '__main__':
     unittest.main()
