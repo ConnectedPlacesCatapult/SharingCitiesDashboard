6762d210d4ae3c22c284092d18a49cdd884cc6c7
Andy Anderson <andy@andyscript.com>
2018-11-13 09:48:04 +0000

    Routing re-enabled

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -44,12 +44,12 @@ class App extends React.Component {
           <Router>
             <React.Fragment>
               <Route component={Header} />
-              <Route path="/" component={DataPage} />
-              {/*<Switch>
+              {/*<Route path="/" component={DataPage} />*/}
+              <Switch>
                 <Route exact path="/" component={DashboardPage} />
                 <Route path="/data" component={DataPage} />
                 <Route path="/build" component={BuilderPage} />
-              </Switch>*/}
+              </Switch>
             </React.Fragment>
           </Router>
         </MuiThemeProvider>
