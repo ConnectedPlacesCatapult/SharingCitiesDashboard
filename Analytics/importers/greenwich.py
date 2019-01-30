70cde301eac2c71e01f7a09c47b9f11d34820e23
thanosbnt <TBantis@futurecities.catapult.org.uk>
2019-01-30 11:07:33 +0000

    added docstring

--- Analytics/importers/greenwich.py
+++ Analytics/importers/greenwich.py
@@ -1,14 +1,111 @@
 '''
-GreenwichMeta and GreenwichOCC Importer
-The file has two importers.
+This importer imports data from Greenwich ArcGIS REST API. 
 
-GreenwichOCC is dependent upon GreenwichMeta, thus GreenwichMeta needs to be imported first 
-before importing GreenwichOCC.
+Description:
+    
+    GreenwichMeta, GreenwichMeta_2, GreenwichOCC, GreenwichOCC_2 import data related to Smart Parking as well
+    as metadata related to the type of parking bays. Specifically, the metadata consist of two layers with similar structure. The only difference is in the baytype values
+    with the first one having "Disabled" and "Pay & Display" while the second "Car Club Bay", "Shared Coach Bays" and
+    "EV Bay". In terms of tables, the API provides attributes related to the status of the sensors.
 
-Both the classes share the same API key.
-The classes doesn't have any bespoke code apart from calling urls and converting them into dataframes.
-Once that is done it calls the create_datasource method of the base class which saves the sensors, attributes,
-location, creates data tables and saves values.
+    The  classes GreenwichOCC and GreenwichOCC_2 are dependent upon GreenwichMeta and GreenwichMeta_2 
+    thus these 'meta' classes needs to be imported first before importing GreenwichOCC and GreenwichOCC_2.
+
+    Both the classes share the same API key.
+    The classes doesn't have any bespoke code apart from calling urls and converting them into dataframes.
+    Once that is done it calls the create_datasource method of the base class which saves the sensors, attributes,
+    location, creates data tables and saves values.
+
+    example responses:
+        gla_gis.gisapdata.sharingcities_smartparking_meta:
+        {
+           "attributes": {
+            "index": 0,
+            "baycount": 1,
+            "baytype": "Disabled",
+            "city": "London",
+            "hassensor": 1,
+            "isunmarked": 0,
+            "latitude": 51.749209039999997,
+            "locationcode": null,
+            "longitude": -1.2592903120000001,
+            "lotcode": "101",
+            "maxstayperiod": null,
+            "operatinghourcode": "Schedule1",
+            "paymentprovidercode": null,
+            "street": "Albion Place",
+            "subzone": "1",
+            "tariffcode": "Disabled",
+            "ward": "1",
+            "zone": "1",
+            "run_time_stamp": 1544003393000,
+            "ESRI_OID": 1
+           },
+           "geometry": {
+            "x": -140183.55629999936,
+            "y": 6754905.6415000036
+           }
+        }
+
+        gla_gis.gisapdata.sharingcities_smartparking_occ:
+        {
+           "attributes": {
+            "free": 2,
+            "isoffline": 0,
+            "lotcode": 135,
+            "occupied": 0,
+            "run_time_stamp": 1539214505000
+           }
+    
+    Importers GreenwichKiwiPump and GreenwichWholeHouse import layer energy related data from Ernest Dence Boiler House and household
+    energy consumption data.
+    It is not clear whether they relate to timeseries or individual sensors, and at the moment these are treated
+    as a single sensor. The data appears to be static and this is reflected on REFRESH_TIME_KIWI and REFRESH_TIME_KIWI_HOUSE.
+    Also note that new Units and Subtheme are needed for these importers.
+
+    example units table:
+
+        id |    _type    | description |         timestamp
+    ----+-------------+-------------+----------------------------
+     1 | kg          | Kilogram    | 2018-12-03 12:54:04.782258
+     2 | g           | Grams       | 2018-12-03 12:54:04.782298
+     3 | km          | KiloMeter   | 2018-12-03 12:54:04.782332
+     4 | kW          | kilowatt    | 2019-01-29 14:12:17.7864
+     5 | lx          | lux         | 2019-01-29 14:16:52.939201
+     6 | temperature | celsius     | 2019-01-29 14:20:13.865564
+    
+    example subtheme table:
+
+        id | t_id |    name    |         timestamp
+    ----+------+------------+----------------------------
+     1 |    1 | Airquality | 2018-12-03 12:54:04.814307
+     2 |    2 | Traffic    | 2018-12-03 12:54:04.81434
+     3 |    1 | Energy     | 2019-01-29 14:13:35.181018
+
+
+    example responses:
+        kiwi_residential_sample_minute
+
+          {
+           "attributes": {
+            "OBJECTID": 1,
+            "time": 1515369600000,
+            "power_wm_sid_761573_wholehouse": 0.23000000000000001,
+            "light_avg_lux_sid_400191_e_room": 6.4400000000000004,
+            "temp_avg_degc_sid_400191_e_room": 21.399999999999999
+           }
+
+        kiwi_ernestdence_boiler_pump_minute
+
+          {
+           "attributes": {
+            "OBJECTID": 6,
+            "site": "Ernest Dence Boiler House",
+            "meter": "Boiler room pumps",
+            "time_": 1515456300000,
+            "value_kw": 19
+           }
+
 '''
 
 import os, sys
