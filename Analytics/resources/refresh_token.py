cc10622f87eb06bc634ff9412a0afe960d868127
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-18 08:10:10 +0200

    Update refresh_token.py

--- Analytics/resources/refresh_token.py
+++ Analytics/resources/refresh_token.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 from flask_jwt_extended import  jwt_refresh_token_required, get_jwt_identity, create_access_token
+from models.users import Users
 
 class TokenRefresh(Resource):
     @jwt_refresh_token_required
