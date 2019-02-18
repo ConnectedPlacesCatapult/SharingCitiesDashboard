615c8225b2db84116e15bbca4f2facb8c7377b4d
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-18 07:59:11 +0200

    Update refresh_token.py

--- Analytics/resources/refresh_token.py
+++ Analytics/resources/refresh_token.py
@@ -5,5 +5,6 @@ class TokenRefresh(Resource):
     @jwt_refresh_token_required
     def post(self):
         current_user = get_jwt_identity() # extracts the users identity from the refresh token
+        current_user = Users.find_by_email(current_user)
         access_token = create_access_token(identity = current_user)
         return {'access_token': access_token}
