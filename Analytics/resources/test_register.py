9a8c1ef65c3ed46d09e7c81ea7ec8c229cf0e7d3
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-18 09:22:27 +0200

    Add files via upload

---
+++ Analytics/resources/test_register.py
@@ -0,0 +1,61 @@
+import pytest 
+from models.users import Users
+from app import create_app
+from db import db
+
+@pytest.fixture()
+def test_client():
+	test_app = create_app(DATABASE_NAME='test_analysis', TESTING=True)
+	testing_client = test_app.test_client()
+
+	test_app_context = test_app.app_context()
+	test_app_context.push()
+
+	yield testing_client
+
+	test_app_context.pop()
+
+@pytest.fixture()
+def dummy_user():
+    not_activated = Users("Not Activated","not_activated@FCC.com",Users.generate_hash("1234".encode("utf8")).decode("utf8"),True,False)
+
+    not_activated.save()
+    not_activated.commit()
+
+    return not_activated
+
+def test_correct_details(test_client, dummy_user):
+    assert dummy_user.activated == False
+    response = test_client.post('/register', data=dict(email=dummy_user.email, password="1234"), follow_redirects=True)
+    assert b"account has been registered" in response.data
+    assert response.status_code == 201
+    assert dummy_user.activated == True
+    db.session.delete(dummy_user)
+    db.session.commit()
+
+def test_incorrect_email(test_client):
+    response = test_client.post('/register', data=dict(email="not_a_user@FCC.com", password="1234"), follow_redirects=True)
+    assert b"not authorised to access Sharing Cities Dashboard" in response.data
+    assert response.status_code == 403
+
+def test_incorrect_password(test_client, dummy_user):
+    assert dummy_user.activated == False
+    response = test_client.post('/register', data=dict(email=dummy_user.email, password="1"), follow_redirects=True)
+    assert b"The password entered does not correspond" in response.data
+    assert response.status_code == 403
+    assert dummy_user.activated == False
+    db.session.delete(dummy_user)
+    db.session.commit()
+
+def test_invalid_inputs(test_client):
+    response = test_client.post('/register', data=dict(email=None, password=None), follow_redirects=True)
+    assert b"This field cannot be blank" in response.data
+    assert response.status_code == 400
+    response = test_client.post('/register', data=dict(email="", password=""), follow_redirects=True)
+    assert b"not authorised to access Sharing Cities Dashboard" in response.data
+    assert response.status_code == 403
+
+
+
+
+
