9b1a4894c80faa65b4eb95f97b35e2eb654d6252
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-18 08:07:16 +0200

    Add files via upload

---
+++ Analytics/resources/test_change_password.py
@@ -0,0 +1,67 @@
+import pytest 
+from models.users import Users
+from app import create_app
+from db import db
+from flask_jwt_extended import create_access_token
+
+@pytest.fixture()
+def test_client():
+	test_app = create_app(DATABASE_NAME='test_analysis', TESTING=True)
+	testing_client = test_app.test_client()
+
+	test_app_context = test_app.app_context()
+	test_app_context.push()
+
+	yield testing_client
+
+	test_app_context.pop()
+
+@pytest.fixture()
+def dummy_user():
+    user = Users("User","user@FCC.com",Users.generate_hash("wfnbqk".encode("utf8")).decode("utf8"),True,True)
+
+    user.save()
+    user.commit()
+
+    return user
+
+
+
+def test_correct_details(test_client, dummy_user):
+    old_password = dummy_user.password
+    access_token = create_access_token(identity = dummy_user)
+    header = {'Authorization': 'Bearer {}'.format(access_token)}
+    response = test_client.patch('/changePassword', data=dict(email=dummy_user.email, password_old="wfnbqk", password_new="abcd"), follow_redirects=True, headers=header)
+    assert b"Password has been updated" in response.data
+    assert response.status_code == 200
+    assert dummy_user.password != old_password
+
+    db.session.delete(dummy_user)
+    db.session.commit()
+
+def test_incorrect_old_password(test_client, dummy_user):
+    old_password = dummy_user.password
+    access_token = create_access_token(identity = dummy_user)
+    header = {'Authorization': 'Bearer {}'.format(access_token)}
+    response = test_client.patch('/changePassword', data=dict(email=dummy_user.email, password_old="123", password_new="abcd"), follow_redirects=True, headers=header)
+    assert b"Incorrect credentials" in response.data
+    assert response.status_code == 403
+    assert dummy_user.password == old_password
+
+    db.session.delete(dummy_user)
+    db.session.commit()
+
+
+def test_invalid_inputs(test_client,dummy_user):
+    access_token = create_access_token(identity = dummy_user)
+    header = {'Authorization': 'Bearer {}'.format(access_token)}
+    response = test_client.patch('/changePassword', data=dict(email=None, password_old=None, password_new=None), follow_redirects=True, headers=header)
+    assert b"This field cannot be blank" in response.data
+    assert response.status_code == 400
+
+    db.session.delete(dummy_user)
+    db.session.commit()
+
+
+
+
