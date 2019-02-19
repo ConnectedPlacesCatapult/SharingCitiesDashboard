d4dbf8699cd14348bd087ae4bb73711b8692a20a
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-19 12:09:26 +0200

    added docstring to app.py

--- Analytics/app.py
+++ Analytics/app.py
@@ -7,11 +7,8 @@ from resources.request_for_data import RequestForData
 from resources.login import Login, SecretResource
 from resources.refresh_token import TokenRefresh
 from resources.logout import UserLogoutAccess, UserLogoutRefresh
-from resources.register import Register
-
 from db import db
 from flask_cors import CORS
-# from flask_bcrypt import Bcrypt
 
 from flask_jwt_extended import JWTManager
 import datetime
@@ -19,7 +16,7 @@ from models.revoked_tokens import RevokedTokens
 
 def create_app(**config_overrides):
     app = Flask(__name__)
-    app.config.from_pyfile('settings.py') #TODO: change name of database in settings
+    app.config.from_pyfile('settings.py')
     app.config.update(config_overrides)
     cors = CORS(app, resources={r"/*": {"origins":"*"}})
     api = Api(app)
@@ -29,31 +26,36 @@ def create_app(**config_overrides):
     app.config['JWT_BLACKLIST_ENABLED'] = True
     app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
-    # password_bcrypt = Bcrypt(app)
     db.init_app(app)
     db.app = app
 
     jwt = JWTManager(app)
     @jwt.token_in_blacklist_loader
     def check_if_token_in_blacklist(decrypted_token):
+        ''' Create a function that will be called whenever an access or 
+            refresh token is required by and sent to an endpoint.
+        '''
         jti = decrypted_token['jti']
         return RevokedTokens.is_jti_blacklisted(jti)
 
-    # Create a function that will be called whenever create_access_token
-    # is used. It will take whatever object is passed into the
-    # create_access_token method, and lets us define what custom claims
-    # should be added to the access token.
+
     @jwt.user_claims_loader
     def add_claims_to_access_token(user):
+        ''' Create a function that will be called whenever create_access_token
+            is used. It will take whatever object is passed into the
+            create_access_token method, and lets us define what custom claims
+            should be added to the access token.
+        '''
         return {'admin': user.admin}
 
 
-    # Create a function that will be called whenever create_access_token
-    # is used. It will take whatever object is passed into the
-    # create_access_token method, and lets us define what the identity
-    # of the access token should be.
     @jwt.user_identity_loader
     def user_identity_lookup(user):
+        ''' Create a function that will be called whenever create_access_token
+            is used. It will take whatever object is passed into the
+            create_access_token method, and lets us define what the identity
+            of the access token should be.
+        '''
         return user.email
 
     
@@ -77,11 +79,12 @@ def create_app(**config_overrides):
     migrate = Migrate(app, db)
     api.add_resource(Analytics, '/analytics')
     api.add_resource(RequestForData, '/data')
+
+
     api.add_resource(Login, '/login')
     api.add_resource(TokenRefresh, '/refreshToken')
     api.add_resource(UserLogoutAccess, '/revokeAccess')
     api.add_resource(UserLogoutRefresh, '/revokeRefresh')
     api.add_resource(SecretResource, '/secret')
-    api.add_resource(Register, '/register')
 
     return app
