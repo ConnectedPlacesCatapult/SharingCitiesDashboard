9b28fd3fee5089532e9d57b48ea4a60a8f03ea7f
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-19 12:02:32 +0200

    added docstring to logout.py

--- Analytics/resources/logout.py
+++ Analytics/resources/logout.py
@@ -3,6 +3,11 @@ from flask_restful import Resource
 from flask_jwt_extended import  jwt_refresh_token_required, jwt_required, get_jwt_identity, create_access_token, get_raw_jwt
 
 class UserLogoutAccess(Resource):
+    """API that revokes a users access JWT by adding it to the revoked_tokens table
+        :required: A valid access JWT in the Authorization Header in the format - Bearer <JWT>
+        :return: A message that indicates whether the token has been revoked 
+        :rtype: JSON
+    """
     @jwt_required
     def post(self):
         jti = get_raw_jwt()['jti']
@@ -15,6 +20,11 @@ class UserLogoutAccess(Resource):
 
 
 class UserLogoutRefresh(Resource):
+    """API that revokes a users refresh JWT by adding it to the revoked_tokens table
+        :required: A valid refresh JWT in the Authorization Header in the format - Bearer <JWT>
+        :return: A message that indicates whether the token has been revoked 
+        :rtype: JSON
+    """
     @jwt_refresh_token_required
     def post(self):
         jti = get_raw_jwt()['jti']
