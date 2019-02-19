cafb819a01c2bba5280bc54e80504d032db18eac
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-19 12:23:33 +0200

    added docstring to revoked_tokens.py

--- Analytics/models/revoked_tokens.py
+++ Analytics/models/revoked_tokens.py
@@ -1,3 +1,7 @@
+'''
+Data model class for storing decrypted access and refresh JWTs that have been revoked 
+'''
+
 from db import db
 
 class RevokedTokens(db.Model):
@@ -6,10 +10,17 @@ class RevokedTokens(db.Model):
     jti = db.Column(db.String(200))
     
     def add(self):
+        """ Adds the current values of the Users fields to SQLAlchemy session and then writes the changes to the database """
         db.session.add(self)
         db.session.commit()
     
     @classmethod
     def is_jti_blacklisted(cls, jti):
+        """ Queries the revoked tokens table to determine if the provided JWT ID is presented and hence has been revoked
+            :param jti: the JWT ID that is used when generating access and refresh JWTs
+            :type jti: string
+            :return: Whether the jti is present in the revoked tokens table
+            :rtype: boolean
+        """
         query = cls.query.filter_by(jti = jti).first()
         return bool(query)
