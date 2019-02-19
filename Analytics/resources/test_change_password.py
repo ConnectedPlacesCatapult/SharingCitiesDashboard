1df2b67a82b9237b2bf5e28322bd59c222463cf3
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-19 12:30:41 +0200

    added docstring to test_change_password.py

--- Analytics/resources/test_change_password.py
+++ Analytics/resources/test_change_password.py
@@ -6,6 +6,9 @@ from flask_jwt_extended import create_access_token
 
 @pytest.fixture()
 def test_client():
+	""" A fixture that initialises the Flask application, saves the current application context for the duration of a single
+        test and yields a testing client that can be used for making requests to the endpoints exposed by the application
+    	"""
 	test_app = create_app(DATABASE_NAME='test_analysis', TESTING=True)
 	testing_client = test_app.test_client()
 
@@ -18,6 +21,8 @@ def test_client():
 
 @pytest.fixture()
 def dummy_user():
+	""" A fixture that creates and saves a user to the database that is to be used for testing purposes """
+	
     user = Users("User","user@FCC.com",Users.generate_hash("wfnbqk".encode("utf8")).decode("utf8"),True,True)
 
     user.save()
@@ -28,6 +33,9 @@ def dummy_user():
 
 
 def test_correct_details(test_client, dummy_user):
+	""" Tests whether the success response message and response code is returned when a user submits the correct email and password
+	    as well as whether the password has been updated in the database  
+	"""
     old_password = dummy_user.password
     access_token = create_access_token(identity = dummy_user)
     header = {'Authorization': 'Bearer {}'.format(access_token)}
@@ -40,6 +48,9 @@ def test_correct_details(test_client, dummy_user):
     db.session.commit()
 
 def test_incorrect_old_password(test_client, dummy_user):
+	""" Tests whether the unsuccessful response message and response code is returned when a user submits the incorrect email and password
+	    as well as whether there is no change to the user's password in the database 
+	"""
     old_password = dummy_user.password
     access_token = create_access_token(identity = dummy_user)
     header = {'Authorization': 'Bearer {}'.format(access_token)}
@@ -53,6 +64,7 @@ def test_incorrect_old_password(test_client, dummy_user):
 
 
 def test_invalid_inputs(test_client,dummy_user):
+	""" Test whether the /changePassword endpoint provides adequate error responses when invalid input is sent to it """
     access_token = create_access_token(identity = dummy_user)
     header = {'Authorization': 'Bearer {}'.format(access_token)}
     response = test_client.patch('/changePassword', data=dict(email=None, password_old=None, password_new=None), follow_redirects=True, headers=header)
@@ -61,7 +73,6 @@ def test_invalid_inputs(test_client,dummy_user):
 
     db.session.delete(dummy_user)
     db.session.commit()
-
 
 
 
