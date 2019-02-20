5fd1df1c854bb1c3aa44115642b224df3f1ffe16
jamesseth <james_seth@yahoo.com>
2019-02-20 13:02:56 +0200

    Docstring added to user model

--- Analytics/models/users.py
+++ Analytics/models/users.py
@@ -1,13 +1,12 @@
-'''
-Data model class for User credentials 
-'''
-
-import json
-from db import db
 from datetime import datetime
-from sqlalchemy.exc import IntegrityError
-import bcrypt
 
+import bcrypt
+from sqlalchemy.exc import IntegrityError
+
+from db import db
+
+
+# TODO: add doc string
 
 class Users(db.Model):
     __tablename__ = 'users'
@@ -15,7 +14,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(400), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(400), nullable = False)
+    password = db.Column(db.String(400), nullable=False)
     admin = db.Column(db.Boolean)
     activated = db.Column(db.Boolean)
     timestamp = db.Column(db.DateTime)
@@ -39,17 +38,18 @@ class Users(db.Model):
         return self.json()
 
     def json(self):
-        """ Returns a JSON representation of Users attributes """
         return {
             'id': self.id,
-            'fullname': self.firstname,
+            'fullname': self.fullname,
             'email': self.email,
             'admin': self.admin,
             'activated': self.activated
         }
 
     def save(self):
-        """ Adds the current values of the Users fields to SQLAlchemy session but does not write to the database """
+        """
+        Add the current Users fields to the SQLAlchemy session
+        """
         try:
             db.session.add(self)
             db.session.flush()
@@ -57,40 +57,36 @@ class Users(db.Model):
             db.session.rollback()
             print(self.fullname, 'User already exists')
 
-    def commit(self):
-        """ Writes all updates done via db.session.add() or manual update of the model class fields to the database """
+    def delete(self):
+        """
+        Add the current Users fields to the SQLAlchemy session to be delete
+        """
+        try:
+            db.session.delete(self)
+            db.session.flush()
+        except IntegrityError as ie:
+            db.session.rollback()
+
+    @staticmethod
+    def commit():
+        """ Commit updated items to the database """
         db.session.commit()
 
     @classmethod
     def find_by_email(cls, email):
-        """ Queries the users table according the email attribute
-            :param email: the email address of the user that is being looked up
-            :type email: string
-            :return: a Users instance where the contained attributes correspond to that of the email provided
-            :rtype: Instance of Users model class 
-        """
-        return cls.query.filter_by(email = email).first()
+        """ Return the user corresponding to the email argument from the Users table """
+
+        return cls.query.filter_by(email=email).first()
 
     @staticmethod
     def generate_hash(password):
-        """A method that uses the provided password and additional salt in order to generate a hash that is to be stored in the DB
-        :param password: the plaintext user password
-        :type password: UTF8 encoded string
-        :return: a hash of the password. The password has salt appeneded to it before it is hashed
-        :rtype: string
-        """    
+        """ Generate a secure hash """
+
         return bcrypt.hashpw(password, bcrypt.gensalt())
-    
+
     @staticmethod
     def verify_hash(password, hash):
-        """A method that verifies whether the password provided matches the corresponding password stored in the database
-        
-        :param password: the plaintext user password
-        :param hash: the user's hashed password that is stored in the user table
-        :type password: UTF8 encoded string
-        :type password: UTF8 encoded string
-        :return: whether the password, when hashed, corresponds to the password hash stored in the users table
-        :rtype: boolean
-        """
+        """ Verify password matches the hashed password in the database. """
+
         return bcrypt.checkpw(password, hash)
 
