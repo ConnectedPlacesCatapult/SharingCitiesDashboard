a351cec0d7117eb54ea6264a2276c03fd5ddd63a
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-20 15:18:56 +0200

    added register resource class

--- Analytics/resources/register.py
+++ Analytics/resources/register.py
@@ -3,30 +3,37 @@ from db import db
 from models.users import Users
 from datetime import datetime
 
-#TODO: add doc string 
-
 class Register(Resource):
+	"""API which allows users to activate their Shared Cities Dashboard account
+	   Parameters can be passed using a POST request that contains a JSON of the following fields:
+        :param fullname: users fullname
+        :param email: users email address
+        :param password: users password that was sent when they were added on the admin page
+        :type fullname: string
+        :type email: string
+        :type password: string
+        :return: A message that indicates whether a user has been registered. If they have not, the message indicates why not
+        :rtype: JSON
+	"""     
+
 	parser = reqparse.RequestParser()
 	parser.add_argument('fullname', type=str, store_missing=False) 
 	parser.add_argument('email', type=str, store_missing=False, help = 'This field cannot be blank', required = True)
 	parser.add_argument('password', type=str, store_missing=False,  help = 'This field cannot be blank', required = True)
+	parser.add_argument('password_new', type=str, store_missing=False,  help = 'This field cannot be blank', required = True)
 
 	def post(self):
 		args = self.parser.parse_args()
 		current_user = Users.find_by_email(args['email']) #t
+
 		if not current_user:
-			return {'message': 'User {} is not authorised to access Sharing Cities Dashboard'. format(args['email'])}, 403 #t
+			return {'message': 'User {} is not authorised to access Sharing Cities Dashboard'. format(args['email'])}, 403 
 
-		# TODO: test hasing pswd store NOTE: # current_user.password = Users.generate_hash(current_user.password.encode("utf8")).decode("utf8");current_user.commit()
+		if not Users.verify_hash(args['password'].encode("utf8"), current_user.password.encode("utf8")):
+			return {'message': 'The password entered does not correspond to the password sent to {}. Please try again'. format(args['email'])}, 403
 
-		# if not User.verify_hash(args['password'], current_user.password):
-		# 	return {'message': 'The password entered does not correspond to the password sent to {}. Please try again'. format(args['email'])}, 403
-
-		if not args['password'] == current_user.password:
-			return {'message': 'The password entered does not correspond to the password sent to {}. Please try again'. format(args['email'])}, 403 #t
-
-
-		current_user.activated = True #t
+		current_user.activated = True 
+		current_user.password = Users.generate_hash(args["password_new"].encode("utf8")).decode("utf8")
 		current_user.commit()
 		return {'message': '{}\'s account has been registered. Redirect to login'. format(args['email'])}, 201 #t
 
