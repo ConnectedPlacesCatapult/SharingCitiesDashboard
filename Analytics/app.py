e7be0f42f55b1858dba919c45e976831c4c6c220
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-21 15:48:29 +0200

    removed unnecessary whitespaces

--- Analytics/app.py
+++ Analytics/app.py
@@ -57,25 +57,20 @@ def create_app(**config_overrides):
     # # for how safely store JWTs in cookies
     # app.config['JWT_COOKIE_CSRF_PROTECT'] = True
 
-
     app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'  # TODO: change before deployement
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(weeks=1)  # TODO: change before deployement
-
     app.config['JWT_BLACKLIST_ENABLED'] = True
     app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
-
-    # password_bcrypt = Bcrypt(app)
+    
     db.init_app(app)
     db.app = app
-
+    
     jwt = JWTManager(app)
-
     @jwt.token_in_blacklist_loader
     def check_if_token_in_blacklist(decrypted_token):
         jti = decrypted_token['jti']
         return RevokedTokens.is_jti_blacklisted(jti)
 
-   
     @jwt.user_claims_loader
     def add_claims_to_access_token(user):
         """ Add admin claim to access token 
