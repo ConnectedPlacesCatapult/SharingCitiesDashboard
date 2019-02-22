302d81386485e47465cc0a1e0d69ac6394329479
James <james_seth@yahoo.com>
2019-02-22 10:30:10 +0200

    Docstrings and PEP8 corrections

--- Analytics/resources/Widgets/get_widgets.py
+++ Analytics/resources/Widgets/get_widgets.py
@@ -1,30 +1,34 @@
 from http import HTTPStatus
 
-from flask import jsonify
-from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
-from flask_restful import abort, inputs
+from flask_restful import abort
 from flask_restful import reqparse
-from sqlalchemy import exc
 
-from db import db
-from models.users import Users
 from models.widget import WidgetModel
 
 
 class GetWidgets(Resource):
     """
-        Get one or more widgets from the database with a userID
+    Get one or more widgets from the database with a userID
+    Parameters can be passed using a POST request that contains a JSON with the following fields:
+    :param  userID: Unique user identification number
+    :param  limit: the max count of widgets to be returned (optional)
 
+    :type userID: Integer
+    :type limit: Integer
+    """
+
+    def __init__(self):
+        """
+        instantiates the get_widgets endpoint
+        Parameters can be passed using a POST request that contains a JSON with the following fields:
         :param  userID: Unique user identification number
         :param  limit: the max count of widgets to be returned (optional)
 
         :type userID: Integer
         :type limit: Integer
-
-    """
-    def __init__(self):
+        """
         # Arguments required to fetch the widgets related to the userID
         self.reqparser_get = reqparse.RequestParser()
         self.reqparser_get.add_argument('userID', required=True, help='A user_id is required',
@@ -34,20 +38,19 @@ class GetWidgets(Resource):
         super().__init__()
 
     @jwt_required
-    def post(self):
-
-        """ Get one or more widgets from the database with a userID
-
-            :param  userID: Unique user identification number
-            :param  limit: the max count of widgets to be returned
-
-            :type userID: Integer
-            :type limit: Integer
-
-            :returns [widget]: A list of widgets with a maximum length of limit and a status code 200
-            :rtype List(widget): A list of widgets
+    def post(self) -> tuple:
         """
+        Get one or more widgets from the database with a userID
+        Parameters can be passed using a POST request that contains a JSON with the following fields:
+        :param  userID: Unique user identification number
+        :param  limit: the max count of widgets to be returned
 
+        :type userID: Integer
+        :type limit: Integer
+
+        :returns [widget]: A list of widgets with a maximum length of limit and a status code 200
+        :rtype List(widget): A list of widgets
+        """
         args = self.reqparser_get.parse_args()
         # Fetch the widget instances related to the userID passed
         widgets = WidgetModel.query.filter_by(user_id=args["userID"]).limit(args["limit"]).all()
