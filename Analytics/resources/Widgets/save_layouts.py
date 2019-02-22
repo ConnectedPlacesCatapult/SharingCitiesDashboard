dc31265d423fa870a9424f53a03fa6da3436c43d
James <james_seth@yahoo.com>
2019-02-22 10:27:14 +0200

    Docstrings and PEP8 corrections

--- Analytics/resources/Widgets/save_layouts.py
+++ Analytics/resources/Widgets/save_layouts.py
@@ -3,49 +3,69 @@ import json
 from flask_restful import Resource
 from flask_restful import abort
 from flask_restful import reqparse
+
 from models.widget import WidgetModel
-
-
 
 
 class SaveWidgetLayout(Resource):
     """
-        Persists a widgets layout to the database
+    Pesists widget layout to the database
+
+    Parameters can be passed using a POST request that contains a JSON with the following fields:
+    :param layouts: layouts to be saved
+    :type layouts: a JSON array of JSON objects containing:
+    :param id: Related widget identification number
+    :type id:   String
+    :param x: x coordinate of widget
+    :type x:   Integer
+    :param y: y coordinate of widget
+    :type y:   Integer
+    :param h: height of widget
+    :type h:   Integer
+    :param w: width of widget
+    :type w:   Integer
+    :param static: static state
+    :type static:   String
+
+    :return: On success a HTTP status code 204, executed successfully with no content is return
+             otherwise, a HTTP status code 404, not found with content containing JSON objects
+             :param error: a message of "Widgets not found"
+             :type String:
+    :rtype: JSON
     """
 
     def __init__(self):
         # Arguments passer to save layouts appends values sent in post to a list
         self.post_reqparser = reqparse.RequestParser()
         self.post_reqparser.add_argument('layouts', action='append')
-
         super().__init__()
 
-    def post(self):
+    def post(self) -> tuple:
         """
-            Pesists widget layout to the database
-            :param layouts: layouts to be saved
-            :type layouts: a JSON array of JSON objects containing:
-                                :param id: Related widget identification number
-                                :type id:   String
-                                :param x: x coordinate of widget
-                                :type x:   Integer
-                                :param y: y coordinate of widget
-                                :type y:   Integer
-                                :param h: height of widget
-                                :type h:   Integer
-                                :param w: width of widget
-                                :type w:   Integer
-                                :param static: static state
-                                :type static:   String
+        Pesists widget layout to the database
+        Parameters can be passed using a POST request that contains a JSON with the following fields:
+        :param layouts: layouts to be saved
+        :type layouts: a JSON array of JSON objects containing:
+        :param id: Related widget identification number
+        :type id:   String
+        :param x: x coordinate of widget
+        :type x:   Integer
+        :param y: y coordinate of widget
+        :type y:   Integer
+        :param h: height of widget
+        :type h:   Integer
+        :param w: width of widget
+        :type w:   Integer
+        :param static: static state
+        :type static:   String
 
-            :return: On success a HTTP status code 204, executed successfully with no content is return
-                     otherwise, a HTTP status code 404, not found with content containing JSON objects
-                     :param error: a message of "Widgets not found"
-                     :type String:
+        :return: On success a HTTP status code 204, executed successfully with no content is return
+                 otherwise, a HTTP status code 404, not found with content containing JSON objects
+                 :param error: a message of "Widgets not found"
+                 :type String:
 
-            :rtype <class 'tuple'>
+        :rtype: JSON
         """
-
         # Keep track of widgetIDs that are not found to inform user
         widgets_not_found = []
         # Keep track of widgetIDs that are updated to inform user
