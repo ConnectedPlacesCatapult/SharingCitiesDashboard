5c8d5c9d74191868bb755296ba7d548ec2d1f185
James <james_seth@yahoo.com>
2019-02-22 10:47:08 +0200

    Docstrings and PEP8 corrections

--- Analytics/resources/admin/user_list.py
+++ Analytics/resources/admin/user_list.py
@@ -1,4 +1,6 @@
+from http import HTTPStatus
 from flask import jsonify
+from flask import abort
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 from flask_restful import fields
@@ -9,15 +11,15 @@ from models.users import Users
 
 
 class UsersList(Resource):
-    """ API resource class that returns a list of users from the database
+    """
+    API resource class that returns a list of users from the database
 
-      Parameters can be passed using a GET request that contains the following fields in the url:
-      :param  limit: the max count of users to be returned
-      :type limit: integer
-      :returns: A list of widgets with a maximum length of limit and a status code 200
-      :rtype: JSON
-  """
-
+    Parameters can be passed using a GET request that contains the following fields in the url:
+    :param  limit: the max count of users to be returned
+    :type limit: integer
+    :returns: A list of widgets with a maximum length of limit and a status code 200
+    :rtype: JSON
+    """
     user_fields = {"id": fields.Integer,
                    "email": fields.String,
                    "fullname": fields.String,
@@ -27,26 +29,31 @@ class UsersList(Resource):
                    }
 
     def __init__(self):
+        """
+        Instantiates the list user endpoint
+        Parameters can be passed using a GET request that contains the following fields in the url:
+        :param  limit: the max count of users to be returned
+        :type limit: integer
+        :returns: A list of widgets with a maximum length of limit and a status code 200
+        :rtype: JSON
+        """
         self.get_reqparser = reqparse.RequestParser()
         self.get_reqparser.add_argument("limit", required=True, help='limit is required', location=['form', 'json'])
 
     # Returns a list of users
     @jwt_required
-    def post(self):
-        """ Get a list of user with from the database
-
-            :param  limit: the max count of widgets to be returned
-
-            :type limit: Integer
-
-            :returns [widget]: A list of widgets with a maximum length of limit and a status code 200
-            :rtype List(widget): A list of widgets
+    def post(self) -> dict:
         """
-
+        Get a list of user with from the database
+        :param  limit: the max count of widgets to be returned
+        :type limit: Integer
+        :returns: A list of widgets with a maximum length of limit and a status code 200
+        :rtype dict: A dict of widgets
+        """
         args = self.get_reqparser.parse_args()
 
-        # if not get_jwt_claims()['admin']:
-        #     abort(HTTPStatus.FORBIDDEN.value, error="administration privileges required")
+        if not get_jwt_claims()['admin']:
+            abort(HTTPStatus.FORBIDDEN.value, error="administration privileges required")
 
         users = (marshal(user, self.user_fields)
                  for user in Users.query.filter_by().limit(args["limit"]))
