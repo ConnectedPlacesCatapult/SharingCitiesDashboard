fc92d53daf196d8aa564d142872f74110052f3ac
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-22 12:05:26 +0200

    reformatted docstring

--- Analytics/models/revoked_tokens.py
+++ Analytics/models/revoked_tokens.py
@@ -10,16 +10,19 @@ class RevokedTokens(db.Model):
 
     def add(self):
         """ Adds the current values of the Users fields to SQLAlchemy session and then writes the changes to the database """
+        
         db.session.add(self)
         db.session.commit()
 
     @classmethod
     def is_jti_blacklisted(cls, jti):
-        """ Queries the revoked tokens table to determine if the provided JWT ID is presented and hence has been revoked
-            :param jti: the JWT ID that is used when generating access and refresh JWTs
-            :type jti: string
-            :return: Whether the jti is present in the revoked tokens table
-            :rtype: boolean
+        """ 
+        Queries the revoked tokens table to determine if the provided JWT ID is presented and hence has been revoked
+        :param jti: the JWT ID that is used when generating access and refresh JWTs
+        :type jti: string
+        :return: Whether the jti is present in the revoked tokens table
+        :rtype: boolean
         """
+        
         query = cls.query.filter_by(jti = jti).first()
         return bool(query)
