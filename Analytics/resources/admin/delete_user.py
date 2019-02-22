5f9437a7a62e464b9ba908b6c5fceee51f4e4fb9
James <james_seth@yahoo.com>
2019-02-22 10:56:45 +0200

    Docstrings and PEP8 corrections

--- Analytics/resources/admin/delete_user.py
+++ Analytics/resources/admin/delete_user.py
@@ -1,4 +1,3 @@
-
 from http import HTTPStatus
 
 from flask_jwt_extended import get_jwt_claims
@@ -12,25 +11,42 @@ from models.users import Users
 
 
 class DeleteUser(Resource):
+    """
+    API resource class which deletes a user from the database
+    Parameters can be passed using a DELETE request that contains a JSON with the following fields:
+    :required: valid access JWT where the admin claim has to be true
+    :param email: users email address
+    :type email: string
+    :return: An empty string and a 204 response on success or an error message and relevant status code when unsuccessful
+    :rtype: tuple
+    """
 
-    """ API resource class which deletes a user from the database
+    def __init__(self):
+        """
+        Instanciates the delete user endpoint
+        Parameters can be passed using a DELETE request that contains a JSON with the following fields:
+        :required: valid access JWT where the admin claim has to be true
+        :param email: users email address
+        :type email: string
+        :return: An empty string and a 204 response on success or an error message and relevant status code when unsuccessful
+        :rtype: tuple
+        """
+        self.delete_reqparser = reqparse.RequestParser()
+        self.delete_reqparser.add_argument('email', required=True, location=['form', 'json'])
+        super().__init__()
+
+    @jwt_required
+    def post(self) -> tuple:
+        """
+        API resource class which deletes a user from the database
 
         Parameters can be passed using a DELETE request that contains a JSON with the following fields:
         :required: valid access JWT where the admin claim has to be true
         :param email: users email address
         :type email: string
         :return: An empty string and a 204 response on success or an error message and relevant status code when unsuccessful
-        :rtype: String
-    """
-
-    def __init__(self):
-        # Remove User (Delete request parser)
-        self.delete_reqparser = reqparse.RequestParser()
-        self.delete_reqparser.add_argument('email', required=True, location=['form', 'json'])
-        super().__init__()
-
-    @jwt_required
-    def post(self):
+        :rtype: tuple
+        """
         args = self.delete_reqparser.parse_args()
 
         # User needs admin rights to continue
