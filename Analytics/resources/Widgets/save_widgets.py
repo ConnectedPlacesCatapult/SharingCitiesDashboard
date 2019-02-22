2c77e4f1143df118f2614d706e7178c29608a9fe
James <james_seth@yahoo.com>
2019-02-22 10:23:53 +0200

    Docstrings and PEP8 corrections

--- Analytics/resources/Widgets/save_widgets.py
+++ Analytics/resources/Widgets/save_widgets.py
@@ -8,43 +8,51 @@ from flask_restful import reqparse
 from sqlalchemy import exc
 
 from db import db
-from models.widget_layout import Layouts
 from models.users import Users
 from models.widget import WidgetModel
+from models.widget_layout import Layouts
 
 
 class Widgets(Resource):
     """
-        Persists a widget to the database
-        :endpoint url:  '/widgets/create_widget'
+    Saves a widget to the database with a userID
+    Parameters can be passed using a POST request that contains a JSON with the following fields:
+    :param  userID: Unique user identification number
+    :param  data: the widgets data
 
-        :param  data:    widget data to be persisted
-        :type   data:    JSON object
+    :type  userID: Integer
+    :type  data: JSON
 
+    :returns : a message containing the widget id with a satus code 200 on success
+    :rtype: JSON
     """
 
     def __init__(self):
-
+        """
+        Initiates the widgets resource
+        Parameters can be passed using a POST request that contains a JSON with the following fields:
+        :argument data:   widgets JSON data string recieved through a post request
+        :type: RequestParser
+        """
         # Post request arguments parser ( Save widget)
         self.reqparse_post = reqparse.RequestParser()
-        self.reqparse_post.add_argument('data', help='data required',required=True, location=['form', 'json'])
+        self.reqparse_post.add_argument('data', help='data required', required=True, location=['form', 'json'])
 
         super().__init__()
 
-
-
     @jwt_required
-    def post(self):
-        """ Saves a widget to the database with a userID
+    def post(self) -> tuple:
+        """
+        Saves a widget to the database with a userID
+        Parameters can be passed using a POST request that contains a JSON with the following fields:
+        :param  userID: Unique user identification number
+        :param  data: the widgets data
 
-            :param  userID: Unique user identification number
-            :param  data: the widgets data
+        :type  userID: Integer
+        :type  data: JSON
 
-            :type  userID: Integer
-            :type  data: JSON
-
-            :returns : a message containing the widget id with a satus code 200 on success
-            :rtype: <class 'tuple'>
+        :returns : a message containing the widget id with a satus code 200 on success
+        :rtype: tuple
         """
         args = self.reqparse_post.parse_args()
         current_user = Users.find_by_email(get_jwt_identity())
@@ -65,5 +73,3 @@ class Widgets(Resource):
 
         layout.widget_id = new_widget.id
         return "Widget with id: {} saved".format(new_widget.id), HTTPStatus.OK.value
-
-
