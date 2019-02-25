098114a463ebecbdfd3c4483876e9838cd5c838b
Keaton Pennels <47102070+kpennels@users.noreply.github.com>
2019-02-25 09:17:29 +0200

    added typehints for is_jti_blacklisted method

--- Analytics/models/revoked_tokens.py
+++ Analytics/models/revoked_tokens.py
@@ -1,9 +1,12 @@
 '''
 Data model class for storing decrypted access and refresh JWTs that have been revoked 
 '''
+
 from db import db
 
+
 class RevokedTokens(db.Model):
+    
     __tablename__ = 'revoked_tokens'
     id = db.Column(db.Integer, primary_key = True)
     jti = db.Column(db.String(200))
@@ -15,7 +18,7 @@ class RevokedTokens(db.Model):
         db.session.commit()
 
     @classmethod
-    def is_jti_blacklisted(cls, jti):
+    def is_jti_blacklisted(cls, jti: dict) -> bool:
         """ 
         Queries the revoked tokens table to determine if the provided JWT ID is presented and hence has been revoked
         :param jti: the JWT ID that is used when generating access and refresh JWTs
@@ -24,5 +27,5 @@ class RevokedTokens(db.Model):
         :rtype: boolean
         """
         
-        query = cls.query.filter_by(jti = jti).first()
+        query = cls.query.filter_by(jti=jti).first()
         return bool(query)
