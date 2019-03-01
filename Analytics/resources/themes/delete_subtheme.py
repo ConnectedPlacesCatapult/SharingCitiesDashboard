37bb8004f1ee5477c1b9f46e5b55bb247b6b4074
James <james_seth@yahoo.com>
2019-03-01 07:30:59 +0200

    Sub theme creation, rename, get and deletion added: + Resources:     + add_subtheme.py.............Add a new sub theme     + delete_subtheme.py..........Delete an existing sub theme     + get_subtheme.py.............Fetch sub themes or a sub theme     + rename_subtheme.py..........rename an existing sub theme

---
+++ Analytics/resources/themes/delete_subtheme.py
@@ -0,0 +1,48 @@
+from http import HTTPStatus
+
+from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_restful import Resource
+from flask_restful import reqparse
+
+from models.theme import SubTheme
+
+
+class DeleteSubTheme(Resource):
+    """
+    delete an existing sub theme entry in the database
+    """
+
+    def __init__(self) -> None:
+        """
+        Sets the required arguments to be in the POST request
+        """
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('name', required=False, store_missing=False, type=str, location=['form', 'json'])
+        self.reqparser.add_argument('id', required=False, store_missing=False, type=str, location=['form', 'json'])
+
+    @jwt_required
+    def post(self) -> ({str: str}, HTTPStatus):
+        """
+        Delete an existing sub theme entry in the database.
+        :post_argument  name: the name of the sub theme to be deleted.
+        :post_argument id: id of the sub theme to be deleted.
+        :post_type  name: str
+        :post_type  id: str
+        """
+        if not get_jwt_claims()['admin']:
+            return {"error": "administration privileges required"}, HTTPStatus.FORBIDDEN
+
+        # Get arguments
+        args = self.reqparser.parse_args()
+
+        # does the theme exist?
+        subtheme = SubTheme.get_by_name(args["name"]) if "name" in args else SubTheme.get_by_id(args["id"])
+        if not subtheme:
+            # cannot delete a theme that does not exist.
+            return {'error': 'Sub-theme does not exists.', 'id': " ", 'name': args["name"]}, HTTPStatus.BAD_REQUEST
+
+        # delete the theme
+        subtheme.delete()
+        subtheme.commit()
+
+        return "", HTTPStatus.NO_CONTENT
