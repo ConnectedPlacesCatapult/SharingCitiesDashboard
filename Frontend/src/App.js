663a41ba52ccc6da6ee3c812a8893fcb82b144b2
Patrick Eriksen <patrick@dotmodus.com>
2019-03-04 17:12:45 +0200

    Tesing NPM cors

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -18,6 +18,20 @@ import SnackBar from "./components/common/SnackBar/SnackBar"
 import { connect } from 'react-redux';
 import { fetchConfig } from "./actions/configActions";
 
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
 // default/fallback font
 require('typeface-roboto');
 
