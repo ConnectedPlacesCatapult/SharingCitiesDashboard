5fc418ebd934f36cd2cdc8bb47a5e9fd87853089
James <james_seth@yahoo.com>
2019-03-06 10:01:04 +0200

    Add Theme id argument requirement to delete a SubTheme     mod: theme_id argument added to reqpaser         + Added theme_id to reqparser to discern between SubThemes with           with the same name
        mod: Param theme_id added to docString
            + Additional docString param and type for theme_id added
    
        mod: Logic conditionals added to handle side cases with new
             theme_id argument
             + Check the theme_id is present when deleting SubTheme by name
             + HTTP status code changed to 404 when SubTheme is not found

--- Analytics/resources/themes/delete_subtheme.py
+++ Analytics/resources/themes/delete_subtheme.py
@@ -19,15 +19,19 @@ class DeleteSubTheme(Resource):
         self.reqparser = reqparse.RequestParser()
         self.reqparser.add_argument('name', required=False, store_missing=False, type=str, location=['form', 'json'])
         self.reqparser.add_argument('id', required=False, store_missing=False, type=str, location=['form', 'json'])
+        self.reqparser.add_argument('theme_id', required=False, store_missing=False, type=str,
+                                    location=['form', 'json'])
 
     @jwt_required
     def post(self) -> ({str: str}, HTTPStatus):
         """
         Delete an existing SubTheme.
-        :param  name: the name of SubTheme..
+        :param  name: the name of SubTheme.
         :param id: id of SubTheme.
+        :param theme_id: Parent theme id.
         :type  name: str
         :type  id: str
+        :type theme_id: str
         :returns: A no content with a http status code of 204, otherwise a JSON of the error details
                   and the appropriate http status code
         """
@@ -36,12 +40,20 @@ class DeleteSubTheme(Resource):
 
         # Get arguments
         args = self.reqparser.parse_args()
-
+        subtheme = None
         # does the theme exist?
-        subtheme = SubTheme.get_by_name(args["name"]) if "name" in args else SubTheme.get_by(id=args["id"])
+        if "id" in args:
+            subtheme = SubTheme.get_by(id=args["id"])
+        elif "name" in args and "theme_id" in args:
+            subtheme = SubTheme.get_by(name=args["name"], t_id=args["theme_id"])
+
         if not subtheme:
+            if not ("name" in args and "theme_id" in args):
+                return ({"error": "Argument dependency error: deletion by name requires theme_id"},
+                        HTTPStatus.BAD_REQUEST)
+
             # cannot delete a theme that does not exist.
-            return {'error': 'Sub-theme does not exists.'}, HTTPStatus.BAD_REQUEST
+            return {'error': 'Sub-theme does not exists.'}, HTTPStatus.NOT_FOUND
 
         # delete the theme
         subtheme.delete()
