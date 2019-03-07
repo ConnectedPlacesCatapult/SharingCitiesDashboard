8d0e70be30bf6d384507318764fa18f23308663a
Patrick Eriksen <patrick@dotmodus.com>
2019-03-07 12:41:46 +0200

    Added action to get user's details from email on load

--- Frontend/src/actions/userActions.js
+++ Frontend/src/actions/userActions.js
@@ -16,13 +16,29 @@ export const login = (userCredentials, props) => {
       const token = response.data.access_token
       const userName = response.data.fullname
       const userID = response.data.id
+      const userEmail = credentials.email
       localStorage.setItem('token', token)
       localStorage.setItem('userName', userName)
       localStorage.setItem('userID', userID)
+      localStorage.setItem('userEmail', userEmail)
       props.history.push('/')
     })
     .catch((err) => {
       console.log('login failed', err)
+    })
+  };
+};
+
+export const getUser = () => {
+  return (dispatch) => {
+    const user = {
+      email: localStorage.getItem('userEmail')
+    }
+    axiosInstance.post('/admin/get_user_by_email', user).then((response) => {
+      dispatch({
+        type: SET_CURRENT_USER,
+        payload: response.data,
+      })
     })
   };
 };
