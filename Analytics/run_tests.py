da9bce909a97f8035cc20aef1d5895db962e61b9
MikeRelling <mike.relling@dotmodus.com>
2019-03-14 10:48:34 +0200

    Added unit test for request for data

--- Analytics/run_tests.py
+++ Analytics/run_tests.py
@@ -6,6 +6,8 @@ from resources.test_analytics import AnalyticsTest
 from test_utility import TestUtility
 from resources.test_missing_values import TestMissingValues
 from resources.test_fill_missing_value import TestFillMissingValue
+from resources.test_request_for_data import RequestForDataTestCase
+from resources.attributes.test_attribute_alias import TestAttrAlias
 
 if __name__ == '__main__':
     unittest.main()
