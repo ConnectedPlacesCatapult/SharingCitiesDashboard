3c5451a6cb5241856ec4495525c6d283a477acbc
Keaton <keaton@dotmodus.com>
2019-03-15 10:44:26 +0200

    enforced admin access only to importer_status endpoint

--- Analytics/resources/import_status.py
+++ Analytics/resources/import_status.py
@@ -1,23 +1,27 @@
 from flask_restful import Resource, reqparse, inputs
+from flask_jwt_extended import jwt_required, get_jwt_claims
 from db import db
-
 from models.importer_status import ImporterStatuses
 
 
 class ImportStatus(Resource):
     """
-    API endpoint, return the status of importers and their is a failure,
-    return the reasons for the failure
+    API endpoint, return the status of importers and if their state is
+    failure, return the reasons for the failure
     """
 
+    @jwt_required
     def get(self) -> (str, int):
 
-        statuses = ImporterStatuses.get_all()
-        if statuses:
-            status_list = []
-            for status in statuses:
-                status_list.append(status.json())
+        if get_jwt_claims()["admin"]:
+            statuses = ImporterStatuses.get_all()
+            if statuses:
+                status_list = []
+                for status in statuses:
+                    status_list.append(status.json())
 
-            return status_list, 200
+                return status_list, 200
+            else:
+                return {"message": "No statuses available"}, 404
         else:
-            return {"message": "No statuses available"}, 404
+            return {"message": "Not Authorised"}, 403
