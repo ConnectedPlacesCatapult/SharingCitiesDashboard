e4d88effd8fed46363ef41ce9c62793a579b7635
Keaton <keaton@dotmodus.com>
2019-03-22 15:10:58 +0200

    script to add a superuser to the users table

---
+++ Analytics/add_startup_admin.py
@@ -0,0 +1,63 @@
+import logging
+from getpass import getpass
+from datetime import datetime
+
+from flask_script import Command
+import sqlalchemy
+import bcrypt
+from sqlalchemy.exc import IntegrityError, ProgrammingError
+
+import settings
+
+logging.basicConfig(level='INFO')
+logger = logging.getLogger(__name__)
+
+
+class AddStartupAdmin(Command):
+    """
+    Helper Class
+
+    This class can be used to add a superuser
+    A superuser can be added using the following commmand:
+        python manage.py add_superuser
+
+    """
+
+    def __init__(self):
+        """
+        Create a local sqlalchemy engine that connects to the database
+        defined in settings.py
+        """
+        self.engine = sqlalchemy.create_engine(settings.DB_URI)
+
+    def run(self):
+        """
+        Prompt an admin individual for their fullname, email, password and
+        persist these details in the users table
+        """
+
+        print("==================================")
+        print("ADD ADMIN USER")
+        print("Complete the following details ...")
+        print("==================================")
+        fullname = input("Fullname: ")
+        email = input("Email: ")
+        pswd = getpass()
+        pswd = bcrypt.hashpw(pswd.encode("utf8"), bcrypt.gensalt()).decode("utf8")
+        print("==================================")
+
+        sql_text = sqlalchemy.text("insert into users(fullname,email,"
+                                   "password,admin,activated, timestamp) "
+                                   "values('{}','{}','{}',{},{},'{}')".
+                                   format(fullname, email, pswd, True, True,
+                                          datetime.now()))
+        try:
+            with self.engine.connect() as con:
+                con.execute(sql_text)
+
+            print("Successfully added {} .".format(email))
+            logger.info("Added superuser {} to users table".format(email))
+        except IntegrityError:
+            print("Unsuccessful. User {} already exists".format(email))
+        except ProgrammingError as e:
+            print("Unsuccessful on error:\n{}".format(e))
