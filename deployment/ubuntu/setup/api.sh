009ba8fbec1ce9ecb7e222566f9181d4c8719162
Eduan Slabbert <WizardNo.7@gmail.com>
2019-04-04 12:33:22 +0200

    Created and added setup scripts for all other components.

---
+++ deployment/ubuntu/setup/api.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+unset DEBUG
+PREFIX="   -"
+
+# Set environment variables
+export DB_USERNAME="sharingcities"
+export DB_PASSWORD="sharingcities"
+export DATABASE_NAME="analytics"
+export DB_HOST="localhost"
+export MAN_HOST_VALUE="0.0.0.0"
+export GUN_HOST_VALUE="localhost"
+
+# Initialize script
+echo "-- FCC API Setup --"
+
+# Move to Frontend directory
+cd ~/SharingCitiesDashboard/Frontend
+
+# Check if python requirements have been installed
+if [ $PYREQ_INSTALLED = False ]; then
+    # Install python requirements
+    echo "$PREFIX Installing python requirements"
+    pip3 install -r requirements.txt
+    export PYREQ_INSTALLED=True
+fi
+
+# Rename settings.py.bak
+echo "$PREFIX Initializing flask settings"
+mv settings.py.bak settings.py
+
+# Serve the API on port 5000
+# python3 manage.py gunicorn
---
+++ deployment/ubuntu/setup/nginx.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+unset DEBUG
+PREFIX="   -"
+
+# Initialize script
+echo "-- FCC Nginx Setup --"
+
+# Move to root directory
+cd
+
+# Check if Nginx is installed
+echo "$PREFIX Checking for Nginx"
+if [ -z "$(which nginx)" ]; then
+    # Install Nginx
+    echo "   $PREFIX Installing Nginx"
+    sudo apt install nginx
+fi
+echo "   $PREFIX Nginx installed!"
+
+# Allow Nginx profile in firewall
+echo "$PREFIX Allow Nginx in firewall"
+sudo ufw allow 'Nginx HTTP'
+
+# Setup correct Nginx config file
+echo "$PREFIX Setting Nginx config"
+sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bkup
+sudo cp ~/SharingCitiesDashboard/nginx.conf /etc/nginx/nginx.conf
+
+# Check Nginx status
+echo "$PREFIX Checking Nginx status"
+NGINX_STATUS=$(systemctl status nginx | grep active | awk '{ print $2 }')
+if [ $NGINX_STATUS = "active" ]; then
+    # Reload Nginx
+    echo "$PREFIX Restarting Nginx"
+    sudo systemctl reload nginx
+elif [ $NGINX_STATUS = "inactive" ]; then
+    # Reload Nginx
+    echo "$PREFIX Starting Nginx"
+    sudo systemctl start nginx
+fi
