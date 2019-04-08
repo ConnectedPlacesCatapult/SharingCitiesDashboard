a3dc671ab1b68cac194d24323084762f505502c0
Eduan Slabbert <WizardNo.7@gmail.com>
2019-04-08 16:40:59 +0200

    Moved apiRoot value to top level export.

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -3,11 +3,11 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 export const LOCAL_URL = process.env.API_ADDRESS;
 
 module.exports = {
-  apiRoot: process.env.API_ADDRESS,
   devServer: {
    historyApiFallback: true,
    host:'localhost',
    port:'8080',
+   apiRoot: process.env.API_ADDRESS,
   },
   entry: "./src/index.js",
   output: {
