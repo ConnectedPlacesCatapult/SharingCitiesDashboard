7afbbb736ee5c47e18c54c5a5724c64ac55cd12b
James <james_seth@yahoo.com>
2019-05-02 10:16:52 +0200

    + PEP8

--- Analytics/app.py
+++ Analytics/app.py
@@ -6,7 +6,6 @@ from flask_restful import Api
 
 from db import db
 from models.revoked_tokens import RevokedTokens
-from models.attribute_range import AttributeRange
 from resources.Widgets.create_widget_layout import CreateWidgetLayout
 from resources.Widgets.delete_widget import DeleteWidgets
 from resources.Widgets.get_layouts import GetLayouts
@@ -70,10 +69,7 @@ from resources.units.update_unit import UpdateUnitOfMeasure
 from settings.get_config_decorator import GetConfig
 
 
-
-
 def create_app(**config_overrides):
-
     app = Flask(__name__)
     app.config.update(GetConfig.configure('postgres'))
 
