7c01386bdc19309256ca37832d356abe71ed5577
James <james_seth@yahoo.com>
2019-05-13 07:36:44 +0200

    attribute_data.py + Docstring added + PEP8 fixes

--- Analytics/models/attribute_data.py
+++ Analytics/models/attribute_data.py
@@ -1,6 +1,11 @@
 from db import db
 
+
 def ModelClass(tablename):
+    """
+    Decoratively define the schema for an attribute data table and extend
+    if the table exists.
+    """
     table = {
         's_id': db.Column(db.Text, primary_key=True),
         'value': db.Column(db.Text, primary_key=True),
